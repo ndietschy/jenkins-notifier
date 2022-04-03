3798721a4d0dc86a05f2f7a63af221a3b1176656
Nicolas Dietschy <nicolas.dietschy@gmail.com>
2022-04-03 15:17:20 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# jenkins-notifier
+# jenkins-notifier (firefox extension)
+
+## What it does
+
+This extension notify user with sound when Jenkins job is over.
+
+## How to use it
+
+* Download this repository
+* In manifest.json, replace localhost with the adress of your jenkins server
+* Load it as a tmonitorlocalhostemporary extension, follow this guide for example : https://extensionworkshop.com/documentation/develop/temporary-installation-in-firefox/ 
+
+## Requierements
+
+* Open tab with job to watch
+* Allow playing sound from tab
---
+++ jenkins-notifier.js
@@ -0,0 +1,88 @@
+var runningJob = false;
+var played = 0;
+var successAudio = browser.extension.getURL('src/success.mp3');
+var unstableAudio = browser.extension.getURL('src/unstable.mp3');
+var failAudio = browser.extension.getURL('src/fail.mp3');
+var htmlContent = "";
+var title = document.title;
+var waitTime = 3000; // ms à attendre entre chaque recherche dans la page
+var i=0;
+
+function playSound(mp3Url) {
+  	const audio = new Audio(mp3Url);
+  	audio.play();
+}
+
+function analyzeHTML(content){
+	if(content.includes("Success")){
+		playSound(successAudio);
+	}
+	else if(content.includes("Failed")){
+		playSound(failAudio);
+	}
+	else{
+		playSound(unstableAudio);
+	}
+}
+
+function getElementByXpath(path) {
+	return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+  
+function searchPage(){
+
+	if(window.location.href.endsWith("console")){
+		console.log("Console view i=" + i);
+		// Si la page contient Finished
+		console.log((document.documentElement.innerText).indexOf('Finished: SUCCESS'));
+		if ((document.documentElement.textContent || document.documentElement.innerText).indexOf('Finished:') > -1) {
+			console.log("Job Finished !");
+			if(i == 0){
+				played = 1;
+				console.log("Job already finished");
+			}
+			// Si Finished n'a pas été trouvé dans un premier temps (on évite de jouer le son à chaque fois qu'on ouvre un log)
+			else if(i > 0 && played == 0){
+				console.log("play sound")
+				if ((document.documentElement.innerText).indexOf('Finished: SUCCESS') > -1) {
+					playSound(successAudio);				
+				}
+				else if ((document.documentElement.textContent || document.documentElement.innerText).indexOf('Finished: UNSTABLE') > -1) {
+					playSound(unstableAudio);				
+				}
+				else {
+					playSound(failAudio);				
+				}
+				played = 1;
+				var newTitle = '(!) ' + title;
+				document.title = newTitle;
+			}
+		}
+	i++;
+	}
+	else{
+		console.log("Jobs view");
+		if (document.getElementsByClassName("progress-bar ").length > 1) {
+			console.log("Job is in progress");
+			runningJob = true;
+		}
+		else {
+			if(runningJob){
+				// --- Fonctionne pas trop ----
+				//console.log("Récupération du statut du dernier job");
+				//htmlContent = getElementByXpath("//html/body/div[4]/div[1]/div[2]/div[2]/div[2]/table/tbody/tr[2]").innerHTML;
+				//console.log("html content : "+htmlContent);
+				//analyzeHTML(htmlContent);
+				// ----------------------------
+				var newTitle = '(!) ' + title;
+				document.title = newTitle;			
+				playSound(successAudio);
+			}
+			runningJob = false;
+			console.log("No job is running");
+		}
+	}
+	setTimeout(searchPage, waitTime);
+}
+
+searchPage();
